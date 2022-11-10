5fb8876fc2a5163142e8ce18ae4f57f736eccce3
Leah Young <leah.young21@alumni.colostate.edu>
2022-11-10 13:39:25 -0700

    update promise all to access data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,18 +5,24 @@ import './App.css';
 
 const App = () => {
 
-  const [events, setEvents] = useState([])
+  const [seattleRapEvents, setSeattleRapEvents] = useState([])
+  const [seattleRBEvents, setSeattleRBEvents] = useState([])
+  const [portlandRapEvents, setPortlandRapEvents] = useState([])
+  const [portlandRBEvents, setPortlandRBEvents] = useState([])
+
 
   const getPromises = () => {
     let seattleRapEvents = fetchRapData(385)
     let seattleRBEvents = fetchRBData(385)
-    let portlandRapEvents = fetchRapData(362);
+    let portlandRapEvents = fetchRapData(362)
     let portlandRBEvents = fetchRBData(362)
     Promise.all([seattleRBEvents, seattleRapEvents, portlandRBEvents, portlandRapEvents])
-    .then(data => {
-      console.log("hey", data)
-      setEvents(data[0], data[1], data[2], data[3])
-    })
+      .then(data => {
+        setSeattleRBEvents(data[0]._embedded.events)
+        setSeattleRapEvents(data[1]._embedded.events)
+        setPortlandRBEvents(data[2]._embedded.events)
+        setPortlandRapEvents(data[3]._embedded.events)
+      })
   }
 
 
