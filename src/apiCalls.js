741ae94becae21e88baac171a4cbb0c8dfdad951
Leah Young <leah.young21@alumni.colostate.edu>
2022-11-10 13:02:24 -0700

    attempt promise all

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,21 @@
-export const fetchAllData = (eventId, cityId) => {
-    return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationId=${eventId}&dmaId=${cityId}&apikey=RdUo6tjbk6yRqYpxV6mSAOXBA9wnvZ3S`)
+export const fetchRapData = (cityId) => {
+    return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationId=KnvZfZ7vAv1&dmaId=${cityId}&apikey=RdUo6tjbk6yRqYpxV6mSAOXBA9wnvZ3S`)
     .then(response => {
       if (!response.ok) {
         throw new Error(`${response.status}`)
       }
-      console.log(response)
         return response.json()
     });
 };
+
+export const fetchRBData = (cityId) => {
+  return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationId=KnvZfZ7vAee&dmaId=${cityId}&apikey=RdUo6tjbk6yRqYpxV6mSAOXBA9wnvZ3S`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`${response.status}`)
+    }
+      return response.json()
+  });
+};
+
 
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from "react"
 import { LandingPage } from "../LandingPage/LandingPage";
-import { fetchAllData } from "../../apiCalls";
+import { fetchRapData, fetchRBData } from "../../apiCalls";
 import './App.css';
 
 const App = () => {
 
-  const [events, setEvents] = useState('')
+  const [events, setEvents] = useState([])
 
-  const makePromises = () => {
-      let seattleRapData = fetchAllData('KnvZfZ7vAv1', 385);
-      let seattleRBData = fetchAllData('KnvZfZ7vAee', 385);
-      let portlandRapData = fetchAllData('KnvZfZ7vAv1', 362);
-      let portlandRBData = fetchAllData('KnvZfZ7vAee', 362);
-      let vancouverRapData = fetchAllData('KnvZfZ7vAv1', 528);
-      let vancouverRBData = fetchAllData('KnvZfZ7vAee', 528);
-      Promise.all([seattleRBData, seattleRapData, portlandRBData, portlandRapData, vancouverRBData, vancouverRapData])
-      .then(data => setEvents(data))
+  const getPromises = () => {
+    let seattleRapEvents = fetchRapData(385)
+    let seattleRBEvents = fetchRBData(385)
+    let portlandRapEvents = fetchRapData(362);
+    let portlandRBEvents = fetchRBData(362)
+    Promise.all([seattleRBEvents, seattleRapEvents, portlandRBEvents, portlandRapEvents])
+    .then(data => {
+      console.log("hey", data)
+      setEvents(data[0], data[1], data[2], data[3])
+    })
   }
 
+
   useEffect(() =>  {
-    makePromises();
+    getPromises();
   }, [])
 
   return (
