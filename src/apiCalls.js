d5fcba72a53b8c1f6093a7cdcdd21f0502a8c224
Leah Young <leah.young21@alumni.colostate.edu>
2022-11-09 20:50:50 -0700

    attempt promise all call to get data and remove nonexistent icons from manifest.json

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,11 @@
-export const fetchAllData = () => {
-    return fetch('https://app.ticketmaster.com/discovery/v2/events.json?classificationId=KnvZfZ7vAv1&dmaId=385&apikey=RdUo6tjbk6yRqYpxV6mSAOXBA9wnvZ3S')
+export const fetchAllData = (eventId, cityId) => {
+    return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationId=${eventId}&dmaId=${cityId}&apikey=RdUo6tjbk6yRqYpxV6mSAOXBA9wnvZ3S`)
     .then(response => {
-        console.log(response)
+      if (!response.ok) {
+        throw new Error(`${response.status}`)
+      }
+      console.log(response)
         return response.json()
     });
 };
+
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,27 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { LandingPage } from "../LandingPage/LandingPage";
+import { fetchAllData } from "../../apiCalls";
 import './App.css';
 
 const App = () => {
+
+  const [events, setEvents] = useState('')
+
+  const makePromises = () => {
+      let seattleRapData = fetchAllData('KnvZfZ7vAv1', 385);
+      let seattleRBData = fetchAllData('KnvZfZ7vAee', 385);
+      let portlandRapData = fetchAllData('KnvZfZ7vAv1', 362);
+      let portlandRBData = fetchAllData('KnvZfZ7vAee', 362);
+      let vancouverRapData = fetchAllData('KnvZfZ7vAv1', 528);
+      let vancouverRBData = fetchAllData('KnvZfZ7vAee', 528);
+      Promise.all([seattleRBData, seattleRapData, portlandRBData, portlandRapData, vancouverRBData, vancouverRapData])
+      .then(data => setEvents(data))
+  }
+
+  useEffect(() =>  {
+    makePromises();
+  }, [])
+
   return (
    <LandingPage />
   );
