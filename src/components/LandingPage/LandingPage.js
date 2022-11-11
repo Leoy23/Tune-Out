936d219dcd5754e04d9b27dc34539b1481e21b90
Leah Young <leah.young21@alumni.colostate.edu>
2022-11-10 17:35:05 -0700

    add divider on NavBar, create Homepage component, modify link for landingpage

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './LandingPage.css'
 
 export const LandingPage = () => {
@@ -6,7 +7,9 @@ export const LandingPage = () => {
     <div className='landing-page'>
       <h1>Tune Out</h1>
       <div className='explore-btn-section'>
+      <Link to="/home-page">
         <button className='explore-btn'>Explore Events</button>
+      </Link>
       </div>
     </div>
   )
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const NavBar = () => {
+export const NavBar = () => {
   return (
     <nav className='nav-bar'>
       <h1>Tune Out</h1>
       <span className='nav-bar-links'>
-        <NavLink className="inactive" exact to="/">Home</NavLink><NavLink className="inactive" to="/favorites">Favorites</NavLink>
+        <NavLink className="inactive" exact to="/home-page">Home</NavLink> || <NavLink className="inactive" to="/favorites">Favorites</NavLink>
       </span>
     </nav>
   );
 };
 
-export default NavBar
