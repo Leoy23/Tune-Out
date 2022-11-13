3b7a9ab8407ffe6b99829cc9813e6ad0107cd4c0
Leah Young <leah.young21@alumni.colostate.edu>
2022-11-13 15:40:31 -0700

    change param name on apiCall

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,5 +1,5 @@
-export const fetchAllEvents = (eventId, cityId) => {
-    return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationId=${eventId}&dmaId=${cityId}&apikey=RdUo6tjbk6yRqYpxV6mSAOXBA9wnvZ3S`)
+export const fetchAllEvents = (genreId, cityId) => {
+    return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationId=${genreId}&dmaId=${cityId}&apikey=RdUo6tjbk6yRqYpxV6mSAOXBA9wnvZ3S`)
     .then(response => {
       if (!response.ok) {
         throw new Error(`${response.status}`)
