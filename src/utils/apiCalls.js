ea357944d6bd044475d22aca030dfde72aebec7f
Leah Young <leah.young21@alumni.colostate.edu>
2022-11-14 14:27:16 -0700

    change api key

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,6 +1,6 @@
 export const fetchAllEvents = (genreId) => {
   return fetch(
-    `https://app.ticketmaster.com/discovery/v2/events.json?classificationId=${genreId}&dmaId=385&apikey=RdUo6tjbk6yRqYpxV6mSAOXBA9wnvZ3S`
+    `https://app.ticketmaster.com/discovery/v2/events.json?classificationId=${genreId}&dmaId=385&apikey=APMYJDIjieW7AMcm9ln6ShGpyp0fkGrJ`
   ).then((response) => {
     if (!response.ok) {
       throw new Error(`${response.status}`);
